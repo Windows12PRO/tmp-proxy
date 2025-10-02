164889a712a8410994d9a6dd0b23165c8e5327ed
Windows12PRO <tibenko5@gmail.com>
2025-10-02 17:12:06 +0200

    Add files via upload

---
+++ tmp-proxy/api/apievent.js
@@ -0,0 +1,15 @@
+export default async function handler(req, res) {
+  const { id } = req.query; // приклад: ?id=18845
+  if (!id) {
+    return res.status(400).json({ error: "Немає ID події" });
+  }
+
+  try {
+    const response = await fetch(`https://api.truckersmp.com/v2/events/${id}`);
+    const data = await response.json();
+    res.setHeader("Access-Control-Allow-Origin", "*"); // дозволяємо будь-кому звертатись
+    res.status(200).json(data);
+  } catch (e) {
+    res.status(500).json({ error: e.toString() });
+  }
+}
